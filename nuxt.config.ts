70681fa34562eb2c0b2fada032ab9d8e6077c973
Adrià Alós <adriaalos@gmail.com>
2023-06-07 08:59:39 +0200

    Installing some modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true }
+    devtools: { enabled: true },
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/supabase',
+        '@pinia/nuxt',
+        '@nuxt/image-edge',
+        '@nuxtjs/device',
+        '@nuxtjs/robots',
+        '@nuxtjs/google-fonts',
+        '@nuxtjs/color-mode',
+        'nuxt-swiper'
+    ]
 })
